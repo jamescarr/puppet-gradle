560c1788d48a65cbd4e3ff1a40198f3f2fbeeae2
James Carr <james.r.carr@gmail.com>
2012-07-05 22:56:31 -0500

    Default to most recent version.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class gradle {
   include 'openjdk'
 
-  $version = 'gradle-1.0-rc-1'
+  $version = 'gradle-1.0'
   
   Exec {
 	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
